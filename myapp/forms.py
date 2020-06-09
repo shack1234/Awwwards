e624134e36b80cd05d52459dcb13c5ddfda494ed
shack1234 <shadrack.anayo@strathmore.edu>
2020-06-09 22:57:40 +0300

    formated the form document

--- myapp/forms.py
+++ myapp/forms.py
@@ -8,34 +8,29 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-
-
-
+        
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude =['posted_on','profile','user','rating',]
 
+class DesignForm(forms.ModelForm):
+    class Meta:
+        model = DesignRating
+        fields = ['rating', ]
 
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
 
-
 class ContentForm(forms.ModelForm):
     class Meta:
         model = ContentRating
         fields = ['rating',]
-
 
 class UsabilityForm(forms.ModelForm):
     class Meta:
         model = UsabilityRating
         fields = ['rating', ]
 
-
-class DesignForm(forms.ModelForm):
-    class Meta:
-        model = DesignRating
-        fields = ['rating', ]
