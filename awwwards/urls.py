3ea847d3df94c2dc17324f55db70d7a5053edf8c
shack1234 <shadrack.anayo@strathmore.edu>
2020-06-09 23:02:39 +0300

    formated the doc

--- awwwards/urls.py
+++ awwwards/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
 from rest_framework.authtoken.views import obtain_auth_token
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('myapp.urls')),
